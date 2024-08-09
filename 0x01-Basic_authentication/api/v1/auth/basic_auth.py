a8dc24d6b12de0d4cbd50a4280fa23945b0af88c
Desmends27 <desmondmends27@gmail.com>
2024-08-09 23:20:36 +0000

    added space after basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ The basic authentication class """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
         """ Extracts the base 64 string """
         if authorization_header is None:
             return None
@@ -14,4 +15,4 @@ class BasicAuth(Auth):
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        return authorization_header.split('Basic', 1)[1]
+        return authorization_header.split('Basic ', 1)[1]
