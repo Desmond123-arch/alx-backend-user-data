af8c742cacad41e54cd75469cc1b185ac00740a9
Desmends27 <desmondmends27@gmail.com>
2024-08-09 03:42:15 +0000

    class to manange api authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Contains the api authentication class"""
+
+from typing import TypeVar, List
+from flask import request
+
+
+class Auth:
+    """ Authentication class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """  Returns false-path for now"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Return now, request will be a flask object"""
+        return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user object"""
+        return None
