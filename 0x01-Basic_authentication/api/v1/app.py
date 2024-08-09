b1d5ba2b9eab0390bb1649627c1be6a79b9a0726
Desmends27 <desmondmends27@gmail.com>
2024-08-09 02:58:14 +0000

    added unauthorized route

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,14 +11,22 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+print("Blueprint registed")
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handle
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,8 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -24,3 +26,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized',methods = ['GET'], strict_slashes=False)
+def unauth() -> str:
+    """ Calls abort when unauthorized """
+    abort(401)
