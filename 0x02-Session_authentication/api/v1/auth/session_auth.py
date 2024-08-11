6a776ec899c52fd8efc6722984609c9fea651265
Desmends27 <desmondmends27@gmail.com>
2024-08-11 15:39:52 +0000

    new type for auth_type

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Contains the session authentication class """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Contains the session auth class"""
+    pass
