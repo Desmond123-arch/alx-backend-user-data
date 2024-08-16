f995d7c3d17f6f056e296896e76d1292ea7e66a9
Desmends27 <desmondmends27@gmail.com>
2024-08-16 15:14:49 +0000

    annotate a function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """ Returns the first row found in the users """
         try:
             session = self.__session
