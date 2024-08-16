48658487ea86db8065b0c40583f622b6401e68c7
Desmends27 <desmondmends27@gmail.com>
2024-08-16 16:40:58 +0000

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 """ Contains the authentication functionalites"""
+from db import DB
+from user import User
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """ Encrypts the password  and adds salt"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user to the database"""
+        session = self._db._session
+        try:
+            user = session.query(User).filter_by(email=email).one()
+        except NoResultFound:
+            raise ValueError(f"User {email} already exists.")
+        new_user = User(email=email, hashed_password=_hash_password(password))
+        session.commit()
+        return new_user
