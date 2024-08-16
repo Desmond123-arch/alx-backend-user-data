6a6b09af07e9b4cec0c73cf60b7211006371bdb5
Desmends27 <desmondmends27@gmail.com>
2024-08-16 03:20:59 +0000

    db created

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,8 +8,8 @@ class User(Base):
     """ Users class for the database """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
