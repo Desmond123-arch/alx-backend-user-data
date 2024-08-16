821289140ad98541e60591171b417c4d1c236a1b
Desmends27 <desmondmends27@gmail.com>
2024-08-16 02:45:18 +0000

    create a users class

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,1 @@
+Continuation of the user authentication service
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,13 @@
+from base import Base
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    """ Users class for the database """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
